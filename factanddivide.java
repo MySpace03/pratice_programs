a86f3a505d2c2d100a358a30d2981d0ee9a20ec5
MySpace03 <aminshavani@gmail.com>
2023-08-23 09:10:05 +0530

    files

---
+++ factanddivide.java
@@ -0,0 +1,10 @@
+public class factanddivide {
+    public static void main(String[]args) {
+        int fact = 64;
+        int i = 1;
+            for(int j=1;j<fact;j++){
+                i = i*j;
+            }
+        System.out.println(i);
+    }
+}
